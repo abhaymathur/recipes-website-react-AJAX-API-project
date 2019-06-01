a4e7c89e2465cbfbe7206f14ccd4520bec2749a4
Abhay Mathur <mathurabhay22@yahoo.in>
2019-06-01 03:32:03 -0700

    dynamic SingleRecipe with env var (AJAX)

--- src/Pages/SingleRecipe.js
+++ src/Pages/SingleRecipe.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { recipeData } from "../data/tempDetails";
+//import { recipeData } from "../data/tempDetails";
 import { Link } from "react-router-dom";
 
 export default class SingleRecipe extends Component {
@@ -7,10 +7,24 @@ export default class SingleRecipe extends Component {
     super(props);
     const id = this.props.match.params.id;
     this.state = {
-      recipeData: recipeData,
+      recipeData: {},
       id: id,
-      loading: false
+      loading: true
     };
+  }
+
+  async componentDidMount() {
+    const url = `https://www.food2fork.com/api/get?key=${
+      process.env.REACT_APP_API_KEY
+    }&rId=${this.state.id}`;
+    try {
+      const response = await fetch(url);
+      const responseData = await response.json();
+      this.setState({
+        recipeData: responseData.recipe,
+        loading: false
+      });
+    } catch (error) {}
   }
   render() {
     const {
@@ -80,3 +94,7 @@ export default class SingleRecipe extends Component {
     );
   }
 }
+
+SingleRecipe.defaultProps = {
+  recipeData: {}
+};
